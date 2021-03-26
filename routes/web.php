6dab6fd2a782a21ecaa6cefac0f02382f3e41c8d
Abdelrhman <boody200boody@gmail.com>
2021-03-26 16:33:16 +0200

    Fix DB Migrations

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,15 @@ Auth::routes();
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
